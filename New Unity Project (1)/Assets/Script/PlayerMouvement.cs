c809bd73f767ebd2644f233c813474dc6dff7f33
Bastien <Bastien@DESKTOP-6OBPV7J>
2021-01-30 15:08:01 +0100

    premier essai

---
+++ New Unity Project (1)/Assets/Script/PlayerMouvement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class PlayerMouvement : MonoBehaviour
+{
+    public float BasicMove;
+    public float jumpForce;
+
+    private bool isJumping;
+    private bool isgrounded;
+
+    public Transform groundleft;
+    public Transform groundrigth;
+
+    public Rigidbody2D rb;
+    private Vector3 velocity = Vector3.zero;
+
+    void Update()
+    {
+        isgrounded = Physics2D.OverlapArea(groundleft.position, groundrigth.position);
+
+        float horizontalMov = Input.GetAxis("Horizontal") * BasicMove * Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump") && isgrounded)
+        {
+            isJumping = true;
+        }
+
+        Move(horizontalMov);
+    }
+
+    void Move(float _horizontalMov)
+    {
+        Vector3 targetVelocity = new Vector2(_horizontalMov, rb.velocity.y);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
+
+        if (isJumping == true)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            isJumping = false;
+        }
+    }
+}
